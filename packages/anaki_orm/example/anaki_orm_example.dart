a06e9d91623d3318e4586006236eee9610bbe50c
Jacob Moura <jacobaraujo7@gmail.com>
2025-02-13 11:21:31 -0300

    start anaki_ork

---
+++ packages/anaki_orm/example/anaki_orm_example.dart
@@ -0,0 +1,6 @@
+import 'package:anaki_orm/anaki_orm.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/anaki_orm/lib/anaki_orm.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/anaki_orm_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
