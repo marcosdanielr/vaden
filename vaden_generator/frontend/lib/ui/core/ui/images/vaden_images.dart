ee47e2c173331946980cf06d10a7564b7dfb4210
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-17 16:21:34 -0300

    Adds image assets path configuration class

--- vaden_generator/frontend/lib/ui/core/ui/images/vaden_images.dart
+++ vaden_generator/frontend/lib/ui/core/ui/images/vaden_images.dart
@@ -1,1 +1,14 @@
-export 'vaden_images.dart';
+class VadenImage {
+  static const String packageName = 'vaden_images';
+
+  // Base path
+  static const String iconsPath = 'assets/icons';
+  static const String logosPath = 'assets/logo';
+
+  // Logo
+  static const String vadenLogo = '$logosPath/vaden_logo.svg';
+  static const String flutterandoLogo = '$logosPath/flutterando_logo.svg';
+
+  // Icons
+  static const String copyrightIcon = '$iconsPath/copyright_icon.svg';
+}
