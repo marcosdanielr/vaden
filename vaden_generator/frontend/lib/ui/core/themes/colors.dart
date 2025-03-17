0b7885a22302b6ffd7826607cf0c3cd9da4d22d0
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-17 15:14:36 -0300

    Adds color scheme definition for app theme

---
+++ vaden_generator/frontend/lib/ui/core/themes/colors.dart
@@ -0,0 +1,48 @@
+import 'dart:ui';
+
+class AppColors {
+  // Main Colors
+  static const Color primaryColor = Color(0xFF111111);
+  static const Color primaryColor2 = Color(0xFF222222);
+  static const Color secondaryColor = Color(0xFFFFFFFF);
+  static const Color backgroundColor = secondaryColor;
+  static const Color backgroundColor2 = primaryColor;
+  static const Color backgroundError = Color(0xFFDBDBDB);
+  static const Color selectedColor = Color(0xFFE0E0FF);
+  static const Color whiteColor = secondaryColor;
+  static const Color blackColor = Color(0xFF000000);
+  static const Color focusColor = Color(0xFF4D7DD6);
+  static const Color errorColor = Color(0xFFFF0073);
+  static const Color successColor = Color(0xFF34c759);
+  static const Color alertColor = Color(0xFFFFBB00);
+  static const Color headerTableColor = Color(0xFFECECED);
+  static const Color mainCardColor = Color(0xFFF8F8F9);
+  static const Color greyColor = backgroundError;
+  static const Color disabledColor = Color(0xFFB6B6B6);
+  static const Color supportColor = Color(0xFF717171);
+
+  // Text Colors
+  static const Color txtPrimary = primaryColor;
+  static const Color txtSecondary = secondaryColor;
+  static const Color txtBlack = blackColor;
+  static const Color txtLink = focusColor;
+  static const Color txtError = errorColor;
+  static const Color txtSuccess = successColor;
+  static const Color txtAlert = alertColor;
+  static const Color txtGrey = greyColor;
+  static const Color txtDisabled = disabledColor;
+  static const Color txtSupport = supportColor;
+  static const Color txtSupport2 = Color(0xFF737077);
+
+  // Stoke Colors
+  static const Color stkBalck = blackColor;
+  static const Color stkWhite = whiteColor;
+  static const Color stkPrimary = primaryColor;
+  static const Color stkFocus = focusColor;
+  static const Color stkError = errorColor;
+  static const Color stkSuccess = successColor;
+  static const Color stkAlert = alertColor;
+  static const Color stkGrey = greyColor;
+  static const Color stkDisabled = disabledColor;
+  static const Color stkSupport = supportColor;
+}
