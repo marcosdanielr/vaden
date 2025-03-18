66085f012ba1d80f9c4921a57015c0782940bd7a
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-03-18 18:04:42 -0300

    Update client_http.dart

--- vaden_generator/frontend/lib/data/services/client_http.dart
+++ vaden_generator/frontend/lib/data/services/client_http.dart
@@ -1,5 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:frontend/config/constants.dart';
 import 'package:result_dart/result_dart.dart';
+
+Dio dioFactory(Constants constants) {
+  return Dio(BaseOptions(baseUrl: constants.urlBase));
+}
 
 class ClientHttp {
   final Dio _dio;
@@ -7,7 +12,6 @@ class ClientHttp {
   ClientHttp(this._dio);
 
   AsyncResult<ClientResponse> get(ClientRequest request) async {
-    _dio.options.baseUrl = const String.fromEnvironment('BASE_URL');
     try {
       final response =
           await _dio.get(request.path, options: Options(headers: request.data));
@@ -23,7 +27,6 @@ class ClientHttp {
   }
 
   AsyncResult<ClientResponse> post(ClientRequest request) async {
-    _dio.options.baseUrl = const String.fromEnvironment('BASE_URL');
     try {
       final response = await _dio.post(
         request.path,
