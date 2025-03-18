6986da9a42277bd3d146345522818c249a44bc80
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-03-18 18:03:42 -0300

    Update generate_repository.dart

--- vaden_generator/frontend/lib/data/repositories/generate_repository.dart
+++ vaden_generator/frontend/lib/data/repositories/generate_repository.dart
@@ -1,10 +1,9 @@
-import 'package:frontend/domain/dtos/dependency_dto.dart';
-import 'package:frontend/domain/dtos/project_dto.dart';
-import 'package:frontend/domain/dtos/project_link_dto.dart';
+import 'package:frontend/domain/entities/dependency.dart';
+import 'package:frontend/domain/entities/project.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract interface class GenerateRepository {
-  AsyncResult<List<DependencyDTO>> getDependencies();
+  AsyncResult<List<Dependency>> getDependencies();
 
-  AsyncResult<ProjectLinkDTO> create(ProjectDto dto);
+  AsyncResult<Unit> createZip(Project project);
 }
