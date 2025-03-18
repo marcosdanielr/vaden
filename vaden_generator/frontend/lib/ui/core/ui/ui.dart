8fb53ec4fe080a564d6695ef01ad6fb96bb7dcd0
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-18 10:42:29 -0300

    Add exports from theme folder

--- vaden_generator/frontend/lib/ui/core/ui/ui.dart
+++ vaden_generator/frontend/lib/ui/core/ui/ui.dart
@@ -2,3 +2,5 @@ export 'buttons/buttons.dart';
 export 'inputs/inputs.dart';
 export 'images/images.dart';
 export 'cards/card.dart';
+export '../../core/themes/colors.dart';
+export '../../core/themes/theme.dart';
