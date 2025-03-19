7eba44b5212b4b91bfacb6a90c436f20afb9e2f3
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-18 22:32:44 -0300

    Updates import paths to use entities instead of DTOs

--- vaden_generator/frontend/lib/data/repositories/generate_repository.dart
+++ vaden_generator/frontend/lib/data/repositories/generate_repository.dart
@@ -1,6 +1,5 @@
-import '../../domain/dtos/dependency_dto.dart';
-import '../../domain/dtos/project_dto.dart';
-import '../../domain/dtos/project_link_dto.dart';
+import '../../domain/entities/dependency.dart';
+import '../../domain/entities/project.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract interface class GenerateRepository {
