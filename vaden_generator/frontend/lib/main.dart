12dfbfbb82ae288af10ee78d625a33f408ac7ae3
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-03-20 08:49:38 -0300

    Initialize internalization

--- vaden_generator/frontend/lib/main.dart
+++ vaden_generator/frontend/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:localization/localization.dart';
 import 'config/dependencies.dart';
 import 'ui/core/themes/theme.dart';
 import 'ui/generate/generate_page.dart';
@@ -13,11 +15,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    LocalJsonLocalization.delegate.directories = ['lib/i18n'];
+
     return MaterialApp(
       title: 'Vaden generator',
       themeMode: ThemeMode.system,
       theme: lightTheme,
       darkTheme: dartTheme,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        LocalJsonLocalization.delegate,
+      ],
+      supportedLocales: [
+        Locale('pt', 'BR'),
+        Locale('en', 'US'),
+      ],
+      localeResolutionCallback: (locale, supportedLocales) {
+        if (supportedLocales.contains(locale)) {
+          return locale;
+        }
+        if (locale?.languageCode == 'pt') {
+          return Locale('pt', 'BR');
+        }
+
+        return Locale('en', 'US');
+      },
       home: const GeneratePage(),
     );
   }
