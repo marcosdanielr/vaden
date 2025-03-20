d865eedbedf0a9b1757d2f78c8c4b4db6f75889c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-20 14:21:57 -0300

    Enhances text input error handling and display

--- vaden_generator/frontend/lib/config/dependencies.dart
+++ vaden_generator/frontend/lib/config/dependencies.dart
@@ -1,10 +1,11 @@
 import 'package:auto_injector/auto_injector.dart';
-import 'constants.dart';
+
 import '../data/repositories/generate_repository.dart';
 import '../data/repositories/remote_generate_repository.dart';
 import '../data/services/client_http.dart';
 import '../data/services/url_launcher_service.dart';
 import '../ui/generate/viewmodels/generate_viewmodel.dart';
+import 'constants.dart';
 
 final injector = AutoInjector();
 
