046285c91b3f20cdf30adcc90c5fe28b7322e13e
Jacob Moura <jacobaraujo7@gmail.com>
2025-03-21 11:18:31 -0300

    Update frontend assets and configuration for Vaden Generator

--- vaden_generator/frontend/lib/config/dependencies.dart
+++ vaden_generator/frontend/lib/config/dependencies.dart
@@ -6,10 +6,12 @@ import '../data/services/client_http.dart';
 import '../data/services/url_launcher_service.dart';
 import '../main_viewmodels.dart';
 import '../ui/generate/viewmodels/generate_viewmodel.dart';
+import 'constants.dart';
 
 final injector = AutoInjector();
 
 void setupInjection() {
+  injector.add(Constants.new);
   injector.add(dioFactory);
   injector.addSingleton(ClientHttp.new);
   injector.addSingleton(UrlLauncherService.new);
