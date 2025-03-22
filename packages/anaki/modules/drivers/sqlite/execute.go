ec342c290a3767b9802ce66c83387a89b12f9dc8
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-22 00:54:13 -0300

    feat: add sqlite execute

--- packages/anaki/modules/drivers/sqlite/execute.go
+++ packages/anaki/modules/drivers/sqlite/execute.go
@@ -1,9 +1,23 @@
 package sqlite
 
-import "anaki/shared/drivers/interfaces"
+import (
+	"anaki/shared/drivers/errors"
+	"anaki/shared/drivers/interfaces"
+	"fmt"
+)
 
 var _ interfaces.Database = (*SQLiteDriver)(nil)
 
 func (s *SQLiteDriver) Execute(query string, args ...interface{}) (int64, error) {
-	panic("method not implemented")
+	result, err := s.conn.Exec(query, args...)
+	if err != nil {
+		return 0, fmt.Errorf("%w: %v", errors.ErrFailedToExecuteQuery, err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return 0, fmt.Errorf("%w: %v", errors.ErrFailedToExecuteQuery, err)
+	}
+
+	return rowsAffected, nil
 }
