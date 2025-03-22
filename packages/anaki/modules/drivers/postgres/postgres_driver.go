f1d88c4226b96e29d10ad78dac93e3229cefca84
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-21 21:33:51 -0300

    feat: postgres execute method

--- packages/anaki/modules/drivers/postgres/postgres_driver.go
+++ packages/anaki/modules/drivers/postgres/postgres_driver.go
@@ -33,7 +33,7 @@ func (p *PostgresDriver) Execute(query string, args ...interface{}) (int64, erro
 	result, err := p.conn.Exec(context.Background(), query, args...)
 
 	if err != nil {
-		return 0, fmt.Errorf("failed to execute query: %v", err)
+		return 0, fmt.Errorf("%w: %v", errors.ErrFailedToExecuteQuery, err)
 	}
 
 	return result.RowsAffected(), nil
