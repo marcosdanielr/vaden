0fff8f0af9cf4bb54add1ac42c069d94e755000e
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-23 04:14:54 -0300

    chore: add queryrow contract

--- packages/anaki/shared/drivers/interfaces/database_contract.go
+++ packages/anaki/shared/drivers/interfaces/database_contract.go
@@ -3,7 +3,7 @@ package interfaces
 type Database interface {
 	Connect(connectionString string) error
 	Query(query string, args ...interface{}) ([]map[string]interface{}, error)
-	// QueryRow(query string, args ...interface{}) ([]map[string]interface{}, error)
+	QueryRow(query string, args ...interface{}) (map[string]interface{}, error)
 	Execute(query string, args ...interface{}) (int64, error)
 	Close() error
 }
